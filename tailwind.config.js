cfb972f43f93fd66a38a05b9a07a5ef918d070e9
McNeill Shiner <mcneill.shiner@gmail.com>
2023-10-01 08:14:44 -0400

    Formatted header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{html,js}",
-        "./views/*.ejs"
+        "./views/**/*.ejs",
+        "./partials/**/*.ejs"
     ],
   theme: {
     extend: {
